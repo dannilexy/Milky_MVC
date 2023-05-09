29e863a33f5f9d59bafb78f99ec4986e890a1869
Danny <Joshua.Akinyoade@DANGOTE.COM>
2023-05-09 16:30:08 +0200

    Creat Category

---
+++ MilkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MilkyWeb.Data;
+using MilkyWeb.Models;
+
+namespace MilkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext _context)
+        {
+            this._context = _context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var categoryList = await _context.Categories.ToListAsync();
+            return View(categoryList);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order Cannot Exactly match the name");
+            }
+            if (!ModelState.IsValid) {
+                return View();
+            }
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ MilkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MilkyWeb.Models;
+
+namespace MilkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category{Name = "Action", DisplayOrder = 1, Id = 1},
+                new Category{Name = "SciFi", DisplayOrder = 2, Id = 2},
+                new Category{Name = "History", DisplayOrder = 3, Id = 3 }
+                );
+        }
+    }
+}
